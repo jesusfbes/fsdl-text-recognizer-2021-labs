05fece0f5b583ce7ff5d7555b59fa5fcfe07192b
Jesus Fernandez Bes <jfernbes@everis.com>
2021-05-24 17:20:24 +0200

    limit epoch to 1

--- lab9/training/tests/test_run_experiment.sh
+++ lab9/training/tests/test_run_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FAILURE=false
 
-python training/run_experiment.py --data_class=FakeImageData --model_class=CNN --conv_dim=32 --fc_dim=16 --loss=cross_entropy --num_workers=4 --max_epochs=4 || FAILURE=true
+python training/run_experiment.py --data_class=FakeImageData --model_class=CNN --conv_dim=32 --fc_dim=16 --loss=cross_entropy --num_workers=4 --max_epochs=1 || FAILURE=true
 
 if [ "$FAILURE" = true ]; then
   echo "Test for run_experiment.py failed"
